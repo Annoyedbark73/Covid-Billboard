db199231f3cb7183595d5e23eefa46e2fd064bc8
Annoyedbark73 <annoyedbark73@gmail.com>
2020-11-11 14:23:43 -0600

    update 1
    adds Sixfeetapart and WearingMask objects and methods

---
+++ Covid Billboard/src/SixFeetApart.java
@@ -0,0 +1,10 @@
+public class SixFeetApart {
+
+    public void StaySixFeetApart(){
+        Boolean SixApart = true;
+        String six = "6 feet apart! ";
+        System.out.println(" ");
+        System.out.println(six);
+        
+    }
+}
---
+++ Covid Billboard/src/WearingMask.java
@@ -0,0 +1,10 @@
+public class WearingMask {
+
+    
+    public void WearMask() {
+       boolean mask = true; 
+       System.out.println("Wearing Mask!");
+    }
+
+
+}
