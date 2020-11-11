dc8385ccd92f071fc9be36e17287889bfdc7f017
Annoyedbark73 <annoyedbark73@gmail.com>
2020-11-11 08:10:22 -0600

    Updated Description comment
    Comment  updated

--- Covid Billboard/src/Covid.java
+++ Covid Billboard/src/Covid.java
@@ -1,3 +1,8 @@
+/* hey this is code that i might update later Add a GUI mabye in a new wind ow but im still pretty new to programming been doing it 
+for about 2 years i am 12 at the time i am writing this which is 11/11/20  subscribe to my youtube channel and join my discord with 
+this link https://discord.gg/FpyN75MAMH and my youtube link https://www.youtube.com/channel/UCb1VJWocJCm2IApuEJtndog?view_as=subscriber
+All this content is by AnnoyedBark73 all Rights reserved */
+
 public class Covid {
     public static void main(String[] args) throws Exception {
         System.out.println("Hello, Corona!"); //haven't started yet
