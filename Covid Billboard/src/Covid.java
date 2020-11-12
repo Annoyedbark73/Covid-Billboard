0adaa5cf3b47487c0a31a1bf094326671f91a914
Annoyedbark73 <annoyedbark73@gmail.com>
2020-11-12 11:21:51 -0600

    Version 1.1
    Fixed Copyright  and updated README.MD

--- Covid Billboard/src/Covid.java
+++ Covid Billboard/src/Covid.java
@@ -1,7 +1,13 @@
 /* hey this is code that i might update later Add a GUI mabye in a new wind ow but im still pretty new to programming been doing it 
 for about 2 years i am 12 at the time i am writing this which is 11/11/20  subscribe to my youtube channel and join my discord with 
 this link https://discord.gg/FpyN75MAMH and my youtube link https://www.youtube.com/channel/UCb1VJWocJCm2IApuEJtndog?view_as=subscriber
-All this content is by AnnoyedBark73 all Rights reserved */
+ 
+This is free and unencumbered software released into the public domain.
+
+Anyone is free to copy, modify, publish, use, compile, sell, or
+distribute this software, either in source code form or as a compiled
+binary, for any purpose, commercial or non-commercial, and by any
+means. */
 
 public class Covid {
     public static void main(String[] args) throws Exception {
